8e1b069bb6979ee8e8c7b159b855dc19854a7246
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-07-26 23:37:57 +0900

    fix: ログ出力を追加

--- src/reload_plex.sh
+++ src/reload_plex.sh
@@ -3,6 +3,7 @@
 tail -Fn0 /var/log/kern.log | while read line; do
     echo "$line" | grep "usbcore: deregistering interface driver px4_usb" >/dev/null
     if [ $? = 0 ]; then
+        logger -t reload_plex "Detected usbcore: deregistering interface driver px4_usb"
         echo -n "1-1" > /sys/bus/usb/drivers/usb/unbind
         echo -n "2-1" > /sys/bus/usb/drivers/usb/unbind
         echo -n "1-1" > /sys/bus/usb/drivers/usb/bind
