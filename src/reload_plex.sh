ad573cdde7b98af74853be48c73083cdb286c5ca
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-09-12 23:50:20 +0900

    fix: 不要な設定を削除してウェイトを追加

--- src/reload_plex.sh
+++ src/reload_plex.sh
@@ -5,8 +5,8 @@ tail -Fn0 /var/log/kern.log | while read line; do
     if [ $? = 0 ]; then
         logger -t reload_plex "Detected usbcore: deregistering interface driver px4_usb"
         echo -n "1-1" > /sys/bus/usb/drivers/usb/unbind
-        echo -n "2-1" > /sys/bus/usb/drivers/usb/unbind
+        sleep 1
         echo -n "1-1" > /sys/bus/usb/drivers/usb/bind
-        echo -n "2-1" > /sys/bus/usb/drivers/usb/bind
+        sleep 1
     fi
 done
