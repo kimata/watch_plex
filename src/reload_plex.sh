7d5b308906d3fffd89c689b6786a1c7055213b0a
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-10-19 00:14:11 +0900

    fix: デバイスが検出されるバスが変化しても追従できるようにした

--- src/reload_plex.sh
+++ src/reload_plex.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+bus=$(lsusb | grep PXQ3PE5 | head -n 1 | grep -oP 'Bus \K\d+' | sed 's/^0*//')
+
+echo "チューナーを usb${bus}-1 にて検知しました"
+
 tail -Fn0 /var/log/kern.log | while read line; do
     echo "$line" | grep "usbcore: deregistering interface driver px4_usb" >/dev/null
     if [ $? = 0 ]; then
         logger -t reload_plex "Detected usbcore: deregistering interface driver px4_usb"
-        echo -n "1-1" > /sys/bus/usb/drivers/usb/unbind
+        echo -n "${bus}-1" > /sys/bus/usb/drivers/usb/unbind
         sleep 1
-        echo -n "1-1" > /sys/bus/usb/drivers/usb/bind
+        echo -n "${bus}-1" > /sys/bus/usb/drivers/usb/bind
         sleep 1
         echo "録画パソコンでエラーが発生しました．録画するには復旧処置が必要です．" | mail -s "[ERROR] 録画パソコン" record
     fi
